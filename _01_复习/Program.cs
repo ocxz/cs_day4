aab5b5c7a214e6e768d8d3081428f8c8c46b86c3
ocxz <2252779530@qq.com>
2019-07-10 02:42:46 +0800

    开了个头，学了个调试

--- _01_复习/Program.cs
+++ _01_复习/Program.cs
@@ -6,7 +6,39 @@ namespace _01_复习
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region 提示用户不断输入一个正整数，按end结束输入后，显示刚刚输入的最大值
+            //// 提示用户不断输入一个正整数，按end结束输入后，显示刚刚输入的最大值
+            //// 循环体：提示用户输入正整数，且判断最大值
+            //// 循环条件：用户输入的不是end
+
+            //string input = "";
+            //int max = 0;
+            //while (input != "end")
+            //{
+            //    Console.WriteLine("请输入一个数字，输入end我们将显示你输入的最大值");
+            //    input = Console.ReadLine();
+            //    if (input != "end")
+            //    {
+            //        try
+            //        {
+            //            int number = Convert.ToInt32(input);
+            //            if (number > max)
+            //            {
+            //                max = number;
+            //            }
+            //        }
+            //        catch
+            //        {
+            //            Console.WriteLine("输入的字符串不能转换成数字，请重新输入");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("输入end,输入结束，刚刚输入的正整数中，最大值为{0}", max);
+            //        Console.ReadKey();
+            //    }
+            //}  
+            #endregion
         }
     }
 }
---
+++ _02_调试/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _02_调试
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
+            // 程序运行断点出，就不在向下执行了
+            Console.WriteLine("这行代码有可能出错!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
